0e29e0f8b2393ed046c5b786cd827409e93f76dd
stringintech <stringintech@gmail.com>
2024-01-20 02:57:51 +0330

    add utilities to create warm-up major sequences

--- major/sequence.py
+++ major/sequence.py
@@ -2,6 +2,7 @@ import music21
 
 from major.type import Type
 from major.note import Note
+from major.degree import Degree
 
 
 class Sequence:
@@ -15,9 +16,29 @@ class Sequence:
     def append(self, note: Note):
         self.notes.append(note)
 
+    @property
+    def lowest_degree(self) -> Degree:
+        d = Degree.SEVEN
+        for note in self.notes:
+            if not note.rest and note.major_degree.value < d.value:
+                d = note.major_degree
+        return d
+
+    @property
+    def lowest_degree_lowest_octave(self) -> int:
+        o = None
+        d = self.lowest_degree
+        for note in self.notes:
+            if not note.rest and note.major_degree == d and (o is None or note.octave_offset < o):
+                o = note.octave_offset
+        return o
+
     def to_notes(self, major: Type, base_octave: int) -> list[music21.note.Note]:
+        octave_offset_correction = self.lowest_degree_lowest_octave
         notes = []
         for note in self.notes:
+            if not note.rest:
+                note.octave_offset -= octave_offset_correction
             notes.append(note.to_note(major, base_octave))
         return notes
 
--- practice/warm_ups.py
+++ practice/warm_ups.py
@@ -3,30 +3,124 @@ from fractions import Fraction
 from major import Sequence, Degree, Note
 
 
-def create_step_sequence(note_len: Fraction, rest_len: Fraction) -> Sequence:
+def create_step_sequence(note_len: float) -> Sequence:
     seq = Sequence()
-    rest = Note.new_rest(rest_len)
+    f = Fraction.from_float(note_len)
+    rest = Note.new_rest(f)
     for d in [Degree.ONE, Degree.TWO, Degree.THREE, Degree.FOUR,
               Degree.FIVE, Degree.SIX, Degree.SEVEN]:
-        seq.append(Note.new(0, d, note_len))
         seq.append(rest)
+        seq.append(Note.new(0, d, f))
     for _ in range(2):
-        seq.append(Note.new(1, Degree.ONE, note_len))
         seq.append(rest)
+        seq.append(Note.new(1, Degree.ONE, f))
     for d in [Degree.SEVEN, Degree.SIX, Degree.FIVE, Degree.FOUR,
               Degree.THREE, Degree.TWO, Degree.ONE]:
-        seq.append(Note.new(0, d, note_len))
         seq.append(rest)
+        seq.append(Note.new(0, d, f))
     return seq
 
 
-def create_arpeggio_sequence() -> Sequence:
-    pass
+def create_arpeggio_sequence(note_len: float) -> Sequence:
+    seq = Sequence()
+    f = Fraction.from_float(note_len)
+
+    notes = [
+        Note.new(0, Degree.ONE, f),
+        Note.new(0, Degree.THREE, f),
+        Note.new(0, Degree.FIVE, f),
+        Note.new(1, Degree.ONE, f),
+
+        Note.new_rest(f),
+        Note.new(1, Degree.ONE, f),
+        Note.new(1, Degree.THREE, f),
+        Note.new(1, Degree.FIVE, f),
+    ]
+    
+    r = range(len(notes))
+    for i in list(r) + list(reversed(r)):
+        seq.append(notes[i])
+    return seq
 
 
-def create_gravity_sequence() -> Sequence:
-    pass
+def create_gravity_sequence(note_len: float) -> Sequence:
+    base_f = Fraction.from_float(note_len)
+    double_f = Fraction.from_float(2*note_len)
+    rest = Note.new_rest(base_f)
+
+    seq = Sequence()
+
+    seq.append(rest)
+    seq.append(Note.new(1, Degree.ONE, double_f))
+    seq.append(rest)
+
+    seq.append(Note.new(1, Degree.TWO, base_f))
+    seq.append(Note.new(1, Degree.ONE, double_f))
+    seq.append(rest)
+
+    seq.append(Note.new(1, Degree.THREE, base_f))
+    seq.append(Note.new(1, Degree.TWO, base_f))
+    seq.append(Note.new(1, Degree.ONE, base_f))
+    seq.append(rest)
+
+    seq.append(Note.new(1, Degree.FOUR, base_f))
+    seq.append(Note.new(1, Degree.THREE, base_f))
+    seq.append(Note.new(1, Degree.TWO, base_f))
+    seq.append(Note.new(1, Degree.ONE, base_f))
+
+    seq.append(rest)
+    seq.append(Note.new(1, Degree.ONE, double_f))
+    seq.append(rest)
+
+    seq.append(Note.new(0, Degree.SEVEN, base_f))
+    seq.append(Note.new(1, Degree.ONE, double_f))
+    seq.append(rest)
+
+    seq.append(Note.new(0, Degree.SIX, base_f))
+    seq.append(Note.new(0, Degree.SEVEN, base_f))
+    seq.append(Note.new(1, Degree.ONE, base_f))
+    seq.append(rest)
+
+    seq.append(Note.new(0, Degree.FIVE, base_f))
+    seq.append(Note.new(0, Degree.SIX, base_f))
+    seq.append(Note.new(0, Degree.SEVEN, base_f))
+    seq.append(Note.new(1, Degree.ONE, base_f))
+
+    return seq
 
 
-def create_tendency_sequence() -> Sequence:
-    pass
+def create_tendency_sequence(note_len: float) -> Sequence:
+    base_f = Fraction.from_float(note_len)
+    double_f = Fraction.from_float(2 * note_len)
+    rest = Note.new_rest(double_f)
+
+    seq = Sequence()
+
+    seq.append(Note.new(1, Degree.ONE, double_f))
+    seq.append(rest)
+
+    seq.append(Note.new(0, Degree.SEVEN, base_f))
+    seq.append(Note.new(1, Degree.ONE, base_f))
+    seq.append(rest)
+
+    seq.append(Note.new(1, Degree.TWO, base_f))
+    seq.append(Note.new(1, Degree.ONE, base_f))
+    seq.append(rest)
+
+    seq.append(Note.new(1, Degree.THREE, base_f))
+    seq.append(Note.new(1, Degree.FOUR, base_f))
+    seq.append(rest)
+
+    seq.append(Note.new(1, Degree.FIVE, base_f))
+    seq.append(Note.new(1, Degree.SIX, base_f))
+    seq.append(rest)
+
+    seq.append(Note.new(1, Degree.SEVEN, base_f))
+    seq.append(Note.new(2, Degree.ONE, base_f))
+    seq.append(rest)
+
+    seq.append(Note.new(2, Degree.TWO, base_f))
+    seq.append(Note.new(2, Degree.ONE, base_f))
+    seq.append(rest)
+
+    return seq
